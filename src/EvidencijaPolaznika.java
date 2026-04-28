fb3a322fcb94dd6886058b90bd8357fb4a9c8a5e
Gongamesh <kovacevicstjepan02@gmail.com>
2026-04-28 19:42:09 +0200

    Dodan ispis polaznika

--- src/EvidencijaPolaznika.java
+++ src/EvidencijaPolaznika.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+
 public class EvidencijaPolaznika {
 
-    public static void main(String[] args) {
+    public static void ispisPolaznika(ArrayList<Polaznik> evidencija) {
+        for(Polaznik polaznik : evidencija) {
+            System.out.println(polaznik);
+        }
+    }
 
+    public static void main(String[] args) {
+        ArrayList<Polaznik> evidencija = new ArrayList<>();
+
+        evidencija.add(new Polaznik("Janko", "Jankovic", "jankic@admin.com"));
+        evidencija.add(new Polaznik("Marko", "Markovic", "markic@admin.com"));
+        evidencija.add(new Polaznik("Melman", "Zirafa", "zirafa@admin.com"));
+        evidencija.add(new Polaznik("Alex", "Lav", "lav@admin.com"));
+
+        ispisPolaznika(evidencija);
     }
 }
